1c954ab420d7a0f7fa25489d47650f076b7725e0
PaulaSanchez <pausanchezt12@gmail.com>
2024-03-19 21:26:07 -0500

    card design

---
+++ Battleship/app/src/main/java/com/example/battleship/Rules.kt
@@ -0,0 +1,35 @@
+package com.example.battleship
+
+object Rules {
+    val messages: List<Message> = listOf(
+
+        Message(
+            "Titulo 1",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+                    "printer took a galley of type and scrambled it to make a type specimen book.",
+            R.drawable.prueba
+        ),
+        Message(
+            "Titulo 2",
+            "222Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+                    "printer took a galley of type and scrambled it to make a type specimen book.",
+            R.drawable.nuevo
+        ),
+        Message(
+            "Titulo 3",
+            "333Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+                    "printer took a galley of type and scrambled it to make a type specimen book.",
+            R.drawable.michi
+        ),
+        Message(
+            "Titulo 4",
+            "444Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+                    "printer took a galley of type and scrambled it to make a type specimen book.",
+            R.drawable.xd
+        )
+    )
+}
--- Battleship/app/src/main/java/com/example/battleship/RulesActivity.kt
+++ Battleship/app/src/main/java/com/example/battleship/RulesActivity.kt
@@ -1,10 +1,8 @@
 package com.example.battleship
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,22 +10,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.battleship.Rules.messages
 import com.example.battleship.ui.theme.BattleshipTheme
 
 class RulesActivity : ComponentActivity() {
@@ -35,47 +39,91 @@ class RulesActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BattleshipTheme {
-                // A surface container using the 'background' color from the theme
-                myCard()
+                RulesLazy(messages)
             }
         }
     }
 }
 
+data class Message(val title: String, val body: String, val image: Int)
+
 @Composable
-fun myCard(){
+fun RulesLazy(messages: List<Message>) {
+    remember {
+        Rules.messages
+    }
+    LazyColumn(
+    ) {
+        items(messages) { message ->
+            MyCard(message)
+        }
+    }
+}
+
+@Composable
+fun MyCard(message: Message) {
     Column(
-     modifier = Modifier
-         .fillMaxSize()
-         .background(Color.White),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ){
-        Card (
+    ) {
+        Card(
             modifier = Modifier
-                .width(200.dp)
-                .height(270.dp),
+                .padding(horizontal = 25.dp, vertical = 16.dp),
             shape = RoundedCornerShape(16.dp),
             colors = CardDefaults.cardColors(Color.White),
             elevation = CardDefaults.cardElevation(10.dp)
-        ){
-            Column{
-                Image(
-                    painter = painterResource(id = R.drawable.prueba),
-                    contentDescription = "Image",
+        ) {
+            Column {
+                Image(message)
+                Column(
                     modifier = Modifier
-                        .height(150.dp)
-                        .fillMaxWidth(),
-                    contentScale = ContentScale.Crop
-                    )
+                        .padding(16.dp)
+                ) {
+                    Text(message)
+                }
             }
         }
     }
 }
 
-@Preview()
 @Composable
-fun DefaultPreview(){
-    myCard()
+fun Text(message: Message) {
+    Text(
+        text = message.title,
+        fontSize = 28.sp,
+        color = Color(0xFF2334C5),
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Monospace
+    )
+    Text(
+        text = message.body,
+        color = Color(0xFF9FA7EC),
+        fontSize = 18.sp,
+        maxLines = 5,
+        overflow = TextOverflow.Clip,
+        modifier = Modifier
+            .padding(12.dp)
+    )
+}
+
+@Composable
+fun Image(message: Message) {
+    Image(
+        painter = painterResource(id = message.image),
+        contentDescription = "Image",
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    RulesLazy(messages)
 }
 
