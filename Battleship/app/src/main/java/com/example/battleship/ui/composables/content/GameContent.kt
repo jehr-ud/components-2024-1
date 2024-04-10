3bdc4a20476487607aa883e2458f567aad53f09c
Jorge Hernandez <jorghernandez@mineducacion.gov.co>
2024-04-10 12:43:06 -0500

    Ajustar el ancho del tablero

--- Battleship/app/src/main/java/com/example/battleship/ui/composables/content/GameContent.kt
+++ Battleship/app/src/main/java/com/example/battleship/ui/composables/content/GameContent.kt
@@ -1,7 +1,12 @@
 package com.example.battleship.ui.composables.content
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -10,13 +15,25 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun GameContent(modifier: Modifier = Modifier) {
-    LazyColumn(modifier) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
         items(10) { row ->
-            Row {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
                 repeat(10) { col ->
-                    Column {
-                        Button(onClick = { /*TODO*/ }) {
-                            Text(text = "$col")
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .aspectRatio(1f)
+                    ) {
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Text(text = "$row, $col")
                         }
                     }
                 }
